85be4d92fd42bed938de1c440a3e64de32e6bb4c
unknown <gaurabsubedi299@gmail.com>
2023-12-21 16:29:00 +0545

    custom appbar done

--- lib/common/widgets/custom_shapes/container/primary_container.dart
+++ lib/common/widgets/custom_shapes/container/primary_container.dart
@@ -14,19 +14,26 @@ class TPrimaryHeaderContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TCurvedEdgeWidget(
+        child: SizedBox(
+      height: 400,
       child: Container(
-        color: TColors.primary,
-        padding: const EdgeInsets.all(0),
-        child: Stack(
-          children: [
+          color: TColors.primary,
+          child: Stack(children: [
+            // Background Custom shapes
             Positioned(
+              top: -150,
+              right: -250,
               child: TCircularContainer(
-                backgroundColor: TColors.textWhite.withOpacity(0.1),
-              ),
+                  backgroundColor: TColors.textWhite.withOpacity(0.1)),
             ),
-          ],
-        ),
-      ),
-    );
+            Positioned(
+              top: 100,
+              right: -300,
+              child: TCircularContainer(
+                  backgroundColor: TColors.textWhite.withOpacity(0.1)),
+            ),
+            child,
+          ])),
+    ));
   }
 }
--- lib/features/authentication/screens/home/home.dart
+++ lib/features/authentication/screens/home/home.dart
@@ -1,4 +1,8 @@
+import 'package:bg_trader/common/widgets/appbar/appbar.dart';
 import 'package:bg_trader/common/widgets/custom_shapes/container/primary_container.dart';
+import 'package:bg_trader/common/widgets/productscart/cart_menu_icon.dart';
+import 'package:bg_trader/utils/constants/color.dart';
+import 'package:bg_trader/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,15 +10,33 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-              child: Column(
-                children: [],
-              ),
-            )
+                child: Column(children: [
+              TAppBar(
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(TTexts.homeAppbarTitle,
+                        style: Theme.of(context)
+                            .textTheme
+                            .labelMedium!
+                            .apply(color: TColors.grey)),
+                    Text(TTexts.homeAppbarSubTitle,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineSmall!
+                            .apply(color: TColors.white)),
+                  ],
+                ),
+                actions: [
+                  TCartConterIcon(onPressed: () {}, iconColor: TColors.white)
+                ],
+              )
+            ]))
           ],
         ),
       ),
